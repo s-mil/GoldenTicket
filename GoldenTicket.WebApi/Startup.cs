a663b7a169942885a3efb1263efabd87b0f4a096
Drake Lambert <drake.askme2@gmail.com>
2018-03-13 10:43:12 -0500

    added comments

--- GoldenTicket.WebApi/Startup.cs
+++ GoldenTicket.WebApi/Startup.cs
@@ -7,25 +7,42 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace GoldenTicket.WebApi
 {
+    /// <summary>
+    /// The class for starting the application. Gets the HostingEnvironment and Configuration
+    /// </summary>
     public class Startup
     {
         private IHostingEnvironment _hostingEnvironment { get; set; }
+
         private IConfiguration _configuration { get; set; }
 
+        /// <summary>
+        /// Creates a new instance of this class
+        /// </summary>
+        /// <param name="env">The hosting environment</param>
+        /// <param name="config">The configuration settings for the application</param>
         public Startup(IHostingEnvironment env, IConfiguration config)
         {
             _hostingEnvironment = env;
             _configuration = config;
         }
 
+        /// <summary>
+        /// Configures the services for this application
+        /// </summary>
+        /// <param name="services">The service container for this application</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Models.GoldenTicketContext>(options => options.UseSqlite(_configuration["connectionString"]));
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        /// <summary>
+        /// Configures the application pipeline and pre-startup operations
+        /// </summary>
+        /// <param name="app">For configuring the application pipeline</param>
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (_hostingEnvironment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
