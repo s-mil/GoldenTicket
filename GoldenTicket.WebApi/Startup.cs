c5679e97ebc5bf7b69dbc28e872da9550e0adf82
Drake Lambert <drake.askme2@gmail.com>
2018-03-13 10:05:39 -0500

    added db context to DI

--- GoldenTicket.WebApi/Startup.cs
+++ GoldenTicket.WebApi/Startup.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GoldenTicket.WebApi
 {
     public class Startup
     {
+        private IHostingEnvironment _hostingEnvironment { get; set; }
+        private IConfiguration _configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IConfiguration config)
+        {
+            _hostingEnvironment = env;
+            _configuration = config;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Models.GoldenTicketContext>(options => options.UseSqlite(_configuration["connectionString"]));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
