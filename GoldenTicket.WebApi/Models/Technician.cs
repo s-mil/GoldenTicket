21f86b3f0e0108eec30fca8d10a51d4020a286a8
Drake Lambert <drake.askme2@gmail.com>
2018-03-13 11:10:55 -0500

    formatting

--- GoldenTicket.WebApi/Models/Technician.cs
+++ GoldenTicket.WebApi/Models/Technician.cs
@@ -11,14 +11,17 @@ namespace GoldenTicket.WebApi.Models
         /// The Id for the Technician
         /// </summary>
         public Guid Id { get; set; }
+
         /// <summary>
         /// Is the Technician an Admin?
         /// </summary>
         public bool IsAdmin { get; set; }
+
         /// <summary>
         /// The name of the Technician
         /// </summary>
         public string Name { get; set; }
+
         /// <summary>
         /// The Date the technician object was created
         /// </summary>
