06fe7187c2cdec09124cd7a20d8c706e4c0fb7fa
Drake Lambert <drake.askme2@gmail.com>
2018-04-03 13:07:06 -0500

    Added getting one ticket and adding one

--- GoldenTicket.WebApi/Controllers/TicketController.cs
+++ GoldenTicket.WebApi/Controllers/TicketController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using GoldenTicket.WebApi.Models;
@@ -9,18 +10,45 @@ namespace GoldenTicket.WebApi.Controllers
     [Route("tickets")]
     public class TicketController : Controller
     {
-        [HttpGet]
-        public async Task<IActionResult> GetTickets([FromServices] GoldenTicketContext context)
+        private GoldenTicketContext _context;
+
+        public TicketController(GoldenTicketContext context)
         {
-            var orderedTickets = await context.Tickets
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var orderedTickets = await _context.Tickets
                 .OrderByDescending(ticket => ticket.DateAdded)
                 .GroupBy(ticket => ticket.ClientId)
                 .OrderBy(ticketClientGroup => ticketClientGroup.Count())
                 .SelectMany(ticketClientGroup => ticketClientGroup)
                 .OrderByDescending(ticket => ticket.IsUrgent)
+                .OrderByDescending(ticket => ticket.Open)
                 .ToListAsync();
 
             return Ok(orderedTickets);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTicket([FromRoute] Guid id)
+        {
+            var ticket = await _context.Tickets.FindAsync(id);
+            return ticket == null ? NotFound(new Ticket { Id = id }) : Ok(ticket) as IActionResult;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTicketAsync([FromBody] Ticket ticket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Tickets.Add(ticket);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTicket), new {id = ticket.Id}, ticket);
         }
     }
 }
