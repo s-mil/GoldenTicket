a769690eada8e38c5b709a637836a067281d2757
Drake Lambert <drake.askme2@gmail.com>
2018-04-08 10:36:24 -0500

    added open / close to seed data

--- GoldenTicket.WebApi/Models/SeedData.cs
+++ GoldenTicket.WebApi/Models/SeedData.cs
@@ -231,7 +231,8 @@ namespace GoldenTicket.WebApi.Models
                         Description = $"Super awesome ticket {i}",
                         Complexity = i % 3,
                         IsUrgent = randGenerator.Next(0, 5) == 0,
-                        Notes = "Terrible client to work with"
+                        Notes = "Terrible client to work with",
+                        Open = randGenerator.Next(0, 1) == 0
                     });
                 }
             }
