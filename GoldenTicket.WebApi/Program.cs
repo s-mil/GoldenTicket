9d229c933bb5ffc5b142ed9cabf4473b22de4b38
Drake Lambert <drake.askme2@gmail.com>
2018-04-09 11:01:41 -0500

    listening on port 5001

--- GoldenTicket.WebApi/Program.cs
+++ GoldenTicket.WebApi/Program.cs
@@ -19,7 +19,7 @@ namespace GoldenTicket.WebApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:80")
+                .UseUrls("http://*:5001")
                 .UseStartup<Startup>()
                 .Build();
     }
