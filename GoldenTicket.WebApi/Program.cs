a314843afa2432c5cd72459a008fe58e27763fd8
Drake Lambert <drake.askme2@gmail.com>
2018-04-08 20:14:46 -0500

    added listen to all interfaces

--- GoldenTicket.WebApi/Program.cs
+++ GoldenTicket.WebApi/Program.cs
@@ -19,6 +19,7 @@ namespace GoldenTicket.WebApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:80")
                 .UseStartup<Startup>()
                 .Build();
     }
