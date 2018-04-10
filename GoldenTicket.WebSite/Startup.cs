6fad168e9426eba5b245348550c86515d5636bb1
Drake Lambert <drake.askme2@gmail.com>
2018-04-10 12:01:39 -0500

    added new index through razor pages

--- GoldenTicket.WebSite/Startup.cs
+++ GoldenTicket.WebSite/Startup.cs
@@ -14,7 +14,9 @@ namespace GoldenTicket.WebSite
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        { }
+        { 
+            services.AddMvc();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -24,7 +26,7 @@ namespace GoldenTicket.WebSite
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
