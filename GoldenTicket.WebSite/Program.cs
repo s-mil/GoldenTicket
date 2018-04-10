af7a44931e1772460e19a21c979e2d2f18c971df
Drake Lambert <drake.askme2@gmail.com>
2018-04-10 16:02:26 -0500

    using mvc

--- GoldenTicket.WebSite/Program.cs
+++ GoldenTicket.WebSite/Program.cs
@@ -19,6 +19,7 @@ namespace GoldenTicket.WebSite
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:80")
                 .UseStartup<Startup>()
                 .Build();
     }
