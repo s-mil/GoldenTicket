3ce240277c26158da21e1137d42d922a50a2ac8f
Drake Lambert <drake.askme2@gmail.com>
2018-04-10 12:01:52 -0500

    added some bull shit with identity server

--- GoldenTicket.IdentityServer/Startup.cs
+++ GoldenTicket.IdentityServer/Startup.cs
@@ -15,6 +15,7 @@ namespace GoldenTicket.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer().AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,7 @@ namespace GoldenTicket.IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
