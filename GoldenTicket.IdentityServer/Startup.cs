4c1fdf0bda16c6c31f1f899b46fcf6bf6e5a3c4e
Drake Lambert <drake.askme2@gmail.com>
2018-04-11 16:58:09 -0500

    added identity

--- GoldenTicket.IdentityServer/Startup.cs
+++ GoldenTicket.IdentityServer/Startup.cs
@@ -1,20 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using GoldenTicket.IdentityServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GoldenTicket.IdentityServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration _configuration { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of this class
+        /// </summary>
+        /// <param name="config">The configuration settings for the application</param>
+        public Startup(IConfiguration config)
+        {
+            _configuration = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<IdentityContext>(options => options.UseSqlite(_configuration["connectionString"]));
+
+            services.AddIdentity<IdentityUser<Guid>, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
+
             services.AddMvc();
         }
 
