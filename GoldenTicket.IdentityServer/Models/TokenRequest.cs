3ca262248778fc72d132f0497fc1cc4f462ae692
Drake Lambert <drake.askme2@gmail.com>
2018-04-11 18:30:53 -0500

    added token request

---
+++ GoldenTicket.IdentityServer/Models/TokenRequest.cs
@@ -0,0 +1,9 @@
+namespace GoldenTicket.IdentityServer.Models
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
