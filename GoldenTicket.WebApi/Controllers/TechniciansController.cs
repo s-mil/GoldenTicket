0da056fff50c3a4c2217dfa8a99572101f53df61
Drake Lambert <drake.askme2@gmail.com>
2018-04-12 11:05:11 -0500

    removed add and delete

--- GoldenTicket.WebApi/Controllers/TechniciansController.cs
+++ GoldenTicket.WebApi/Controllers/TechniciansController.cs
@@ -19,39 +19,14 @@ namespace GoldenTicket.WebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return Ok(await _context.Technicians.ToListAsync());
+            return Ok(await _context.Set<Technician>().ToListAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get([FromRoute] Guid id)
         {
-            var ticket = await _context.Tickets.FindAsync(id);
-            return ticket == null ? NotFound(new Ticket { Id = id }) : Ok(ticket) as IActionResult;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddAsync([FromBody] Technician technician)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            _context.Technicians.Add(technician);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(Get), new { id = technician.Id }, technician);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> RemoveAsync([FromRoute] Guid id)
-        {
-            var technician = await _context.Technicians.FindAsync(id);
-            if (technician == null)
-            {
-                return NotFound(new Technician { Id = id });
-            }
-            _context.Technicians.Remove(technician);
-            await _context.SaveChangesAsync();
-            return NoContent();
+            var technician = await _context.Set<Technician>().FindAsync(id);
+            return technician == null ? NotFound(new Technician { Id = id }) : Ok(technician) as IActionResult;
         }
     }
 }
