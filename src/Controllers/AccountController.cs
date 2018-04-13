c34e0940c3c39bbe87d669d6b58497016fb57276
Drake Lambert <drake.askme2@gmail.com>
2018-04-12 20:28:18 -0500

    temporary comment

--- src/Controllers/AccountController.cs
+++ src/Controllers/AccountController.cs
@@ -1,156 +1,156 @@
-﻿using System.Threading.Tasks;
-using GoldenTicket.Models;
-using GoldenTicket.Models.AccountViewModels;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿// using System.Threading.Tasks;
+// using GoldenTicket.Models;
+// using GoldenTicket.Models.AccountViewModels;
+// using Microsoft.AspNetCore.Authentication;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Logging;
 
-namespace GoldenTicket.Controllers
-{
-    [Authorize]
-    [Route("[controller]/[action]")]
-    public class AccountController : Controller
-    {
-        private readonly UserManager<Technician> _userManager;
-        private readonly SignInManager<Technician> _signInManager;
-        private readonly ILogger _logger;
+// namespace GoldenTicket.Controllers
+// {
+//     [Authorize]
+//     [Route("[controller]/[action]")]
+//     public class AccountController : Controller
+//     {
+//         private readonly UserManager<Technician> _userManager;
+//         private readonly SignInManager<Technician> _signInManager;
+//         private readonly ILogger _logger;
 
-        public AccountController(UserManager<Technician> userManager, SignInManager<Technician> signInManager, ILogger<AccountController> logger)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
-        }
+//         public AccountController(UserManager<Technician> userManager, SignInManager<Technician> signInManager, ILogger<AccountController> logger)
+//         {
+//             _userManager = userManager;
+//             _signInManager = signInManager;
+//             _logger = logger;
+//         }
 
-        [TempData]
-        public string ErrorMessage { get; set; }
+//         [TempData]
+//         public string ErrorMessage { get; set; }
 
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login(string returnUrl = null)
-        {
-            // Clear the existing external cookie to ensure a clean login process
-            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+//         [HttpGet]
+//         [AllowAnonymous]
+//         public async Task<IActionResult> Login(string returnUrl = null)
+//         {
+//             // Clear the existing external cookie to ensure a clean login process
+//             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-            ViewData["ReturnUrl"] = returnUrl;
-            return View();
-        }
+//             ViewData["ReturnUrl"] = returnUrl;
+//             return View();
+//         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-            if (ModelState.IsValid)
-            {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return RedirectToLocal(returnUrl);
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToAction(nameof(Lockout));
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return View(model);
-                }
-            }
+//         [HttpPost]
+//         [AllowAnonymous]
+//         [ValidateAntiForgeryToken]
+//         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+//         {
+//             ViewData["ReturnUrl"] = returnUrl;
+//             if (ModelState.IsValid)
+//             {
+//                 // This doesn't count login failures towards account lockout
+//                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+//                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+//                 if (result.Succeeded)
+//                 {
+//                     _logger.LogInformation("User logged in.");
+//                     return RedirectToLocal(returnUrl);
+//                 }
+//                 if (result.IsLockedOut)
+//                 {
+//                     _logger.LogWarning("User account locked out.");
+//                     return RedirectToAction(nameof(Lockout));
+//                 }
+//                 else
+//                 {
+//                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+//                     return View(model);
+//                 }
+//             }
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+//             // If we got this far, something failed, redisplay form
+//             return View(model);
+//         }
 
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult Lockout()
-        {
-            return View();
-        }
+//         [HttpGet]
+//         [AllowAnonymous]
+//         public IActionResult Lockout()
+//         {
+//             return View();
+//         }
 
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult Register(string returnUrl = null)
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-            return View();
-        }
+//         [HttpGet]
+//         [AllowAnonymous]
+//         public IActionResult Register(string returnUrl = null)
+//         {
+//             ViewData["ReturnUrl"] = returnUrl;
+//             return View();
+//         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-            if (ModelState.IsValid)
-            {
-                var user = new Technician
-                {
-                    UserName = $"{model.FirstName}.{model.LastName}",
-                    DateAdded = model.DateAdded,
-                    FirstName = model.FirstName,
-                    LastName = model.LastName,
-                    IsAdmin = model.IsAdmin
-                };
-                var result = await _userManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
-                    return RedirectToLocal(returnUrl);
-                }
-                AddErrors(result);
-            }
+//         [HttpPost]
+//         [AllowAnonymous]
+//         [ValidateAntiForgeryToken]
+//         public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+//         {
+//             ViewData["ReturnUrl"] = returnUrl;
+//             if (ModelState.IsValid)
+//             {
+//                 var user = new Technician
+//                 {
+//                     UserName = $"{model.FirstName}.{model.LastName}",
+//                     DateAdded = model.DateAdded,
+//                     FirstName = model.FirstName,
+//                     LastName = model.LastName,
+//                     IsAdmin = model.IsAdmin
+//                 };
+//                 var result = await _userManager.CreateAsync(user, model.Password);
+//                 if (result.Succeeded)
+//                 {
+//                     _logger.LogInformation("User created a new account with password.");
+//                     return RedirectToLocal(returnUrl);
+//                 }
+//                 AddErrors(result);
+//             }
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+//             // If we got this far, something failed, redisplay form
+//             return View(model);
+//         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
-        }
+//         [HttpPost]
+//         [ValidateAntiForgeryToken]
+//         public async Task<IActionResult> Logout()
+//         {
+//             await _signInManager.SignOutAsync();
+//             _logger.LogInformation("User logged out.");
+//             return RedirectToAction(nameof(HomeController.Index), "Home");
+//         }
 
-        [HttpGet]
-        public IActionResult AccessDenied()
-        {
-            return View();
-        }
+//         [HttpGet]
+//         public IActionResult AccessDenied()
+//         {
+//             return View();
+//         }
 
-        #region Helpers
+//         #region Helpers
 
-        private void AddErrors(IdentityResult result)
-        {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
-        }
+//         private void AddErrors(IdentityResult result)
+//         {
+//             foreach (var error in result.Errors)
+//             {
+//                 ModelState.AddModelError(string.Empty, error.Description);
+//             }
+//         }
 
-        private IActionResult RedirectToLocal(string returnUrl)
-        {
-            if (Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            else
-            {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
-            }
-        }
+//         private IActionResult RedirectToLocal(string returnUrl)
+//         {
+//             if (Url.IsLocalUrl(returnUrl))
+//             {
+//                 return Redirect(returnUrl);
+//             }
+//             else
+//             {
+//                 return RedirectToAction(nameof(HomeController.Index), "Home");
+//             }
+//         }
 
-        #endregion
-    }
-}
+//         #endregion
+//     }
+// }
