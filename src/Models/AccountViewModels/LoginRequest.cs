0fc6ce2d6ba628665d86e6be7d821ed9b142cd4f
Drake Lambert <drake.askme2@gmail.com>
2018-04-12 21:46:23 -0500

    added properties

---
+++ src/Models/AccountViewModels/LoginRequest.cs
@@ -0,0 +1,11 @@
+namespace GoldenTicket.Models.AccountViewModels
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
