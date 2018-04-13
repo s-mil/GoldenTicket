e447135911556f857ceb688b1e51eacfb8f761d5
Drake Lambert <drake.askme2@gmail.com>
2018-04-12 23:18:55 -0500

    added ticket model

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,27 +1,41 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using GoldenTicket.Data;
 using GoldenTicket.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GoldenTicket.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private SignInManager<Technician> _signInManager;
 
-        public HomeController(SignInManager<Technician> signInManager)
+        private GoldenTicketContext _context;
+
+        public HomeController(SignInManager<Technician> signInManager, GoldenTicketContext context)
         {
             _signInManager = signInManager;
+            _context = context;
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            if (!_signInManager.IsSignedIn(User))
-            {
-                return RedirectToAction(nameof(AccountController.Login), "Account");
-            }
-            return View();
+            var orderedTickets = await _context.Tickets
+                .OrderByDescending(ticket => ticket.DateAdded)
+                .GroupBy(ticket => ticket.ClientId)
+                .OrderBy(ticketClientGroup => ticketClientGroup.Count())
+                .SelectMany(ticketClientGroup => ticketClientGroup)
+                .OrderByDescending(ticket => ticket.IsUrgent)
+                .OrderByDescending(ticket => ticket.Open)
+                .ToListAsync();
+
+            return View(orderedTickets);
         }
     }
 }
