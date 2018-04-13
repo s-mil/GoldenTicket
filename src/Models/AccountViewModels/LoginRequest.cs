a8b5d9ab4eb4f068c8524d1d346ccfabd0e25581
Drake Lambert <drake.askme2@gmail.com>
2018-04-12 23:19:07 -0500

    added password datatype

--- src/Models/AccountViewModels/LoginRequest.cs
+++ src/Models/AccountViewModels/LoginRequest.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GoldenTicket.Models.AccountViewModels
 {
     public class LoginRequest
     {
         public string Username { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
