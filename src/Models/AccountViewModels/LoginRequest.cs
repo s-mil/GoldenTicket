c9f49902bc2efa42b6ffc6264ce3c7cd3f72d1a5
Drake Lambert <drake.askme2@gmail.com>
2018-04-12 22:50:54 -0500

    naming

--- src/Models/AccountViewModels/LoginRequest.cs
+++ src/Models/AccountViewModels/LoginRequest.cs
@@ -2,7 +2,7 @@ namespace GoldenTicket.Models.AccountViewModels
 {
     public class LoginRequest
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
 
         public string Password { get; set; }
 
