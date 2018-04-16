6046d8291191f4d5034f18fbd4a4888587497185
Drake Lambert <drake.askme2@gmail.com>
2018-04-16 17:10:40 -0500

    added view models

---
+++ src/Models/TicketsViewModels/TechnicianTime.cs
@@ -0,0 +1,9 @@
+namespace GoldenTicket.Models.TicketsViewModels
+{
+    public class TechnicianTime
+    {
+        public Technician Technician { get; set; }
+
+        public TechnicianTicketTime Time { get; set; }
+    }
+}
