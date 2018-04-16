4ad499f15cb17abfbf60000aa3648189b5c2beff
Drake Lambert <drake.askme2@gmail.com>
2018-04-16 16:22:04 -0500

    added clients list to controller returning view

--- src/Controllers/ClientsController.cs
+++ src/Controllers/ClientsController.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using GoldenTicket.Data;
-using GoldenTicket.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using GoldenTicket.Models.ClientsViewModels;
 
 namespace GoldenTicket.Controllers
 {
-    [Route("[controller]")]
-    public class ClientsController : Controller
+    [Authorize]
+    public partial class ClientsController : Controller
     {
         private GoldenTicketContext _context;
 
@@ -18,41 +20,10 @@ namespace GoldenTicket.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> All()
         {
-            return Ok(await _context.Clients.ToListAsync());
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get([FromRoute] Guid id)
-        {
-            var ticket = await _context.Tickets.FindAsync(id);
-            return ticket == null ? NotFound(new Ticket { Id = id }) : Ok(ticket) as IActionResult;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddAsync([FromBody] Client Client)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            _context.Clients.Add(Client);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(Get), new { id = Client.Id }, Client);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> RemoveAsync([FromRoute] Guid id)
-        {
-            var Client = await _context.Clients.FindAsync(id);
-            if (Client == null)
-            {
-                return NotFound(new Client { Id = id });
-            }
-            _context.Clients.Remove(Client);
-            await _context.SaveChangesAsync();
-            return NoContent();
+            var clients = await _context.Clients.GroupJoin(_context.Tickets.Where(ticket => ticket.Open), client => client.Id, ticket => ticket.ClientId, (client, tickets) => new ClientDetails { Client = client, Tickets = tickets.ToList() }).ToListAsync();
+            return View(clients);
         }
     }
 }
