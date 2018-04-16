00611c2dd2504d5d1abf9f01b288f02149ab499d
Jeremy LeJeune <jleje33@lsu.edu>
2018-04-16 18:51:21 -0500

    added comments to technicians controller

--- src/Controllers/TechniciansController.cs
+++ src/Controllers/TechniciansController.cs
@@ -7,22 +7,38 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GoldenTicket.Controllers
 {
+    /// <summary>
+    /// Controller for technicians
+    /// </summary>
     [Route("[controller]")]
     public class TechniciansController : Controller
     {
         private GoldenTicketContext _context;
 
+        /// <summary>
+        /// intializes _context
+        /// </summary>
+        /// <param name="context">context of the technician</param>
         public TechniciansController(GoldenTicketContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Adds tech to index
+        /// </summary>
+        /// <returns>Http OK response</returns>
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             return Ok(await _context.Set<Technician>().ToListAsync());
         }
 
+        /// <summary>
+        /// Gets the tech from id
+        /// </summary>
+        /// <param name="id">Unique id for technician</param>
+        /// <returns>404 error or Http response for success</returns>
         [HttpGet("{id}")]
         public async Task<IActionResult> Get([FromRoute] string id)
         {
