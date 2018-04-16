123b108900dc9da8e36ddb14c43ddc720a287c81
Jeremy LeJeune <jleje33@lsu.edu>
2018-04-16 18:35:05 -0500

    added comments to clients controller

--- src/Controllers/ClientsController.cs
+++ src/Controllers/ClientsController.cs
@@ -9,16 +9,27 @@ using GoldenTicket.Models.ClientsViewModels;
 
 namespace GoldenTicket.Controllers
 {
+    /// <summary>
+    /// Controller for Clients
+    /// </summary>
     [Authorize]
     public partial class ClientsController : Controller
     {
         private GoldenTicketContext _context;
 
+        /// <summary>
+        /// Initializes _context
+        /// </summary>
+        /// <param name="context">context of client</param>
         public ClientsController(GoldenTicketContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Shows all clients
+        /// </summary>
+        /// <returns>clients page</returns>
         [HttpGet]
         public async Task<IActionResult> All()
         {
