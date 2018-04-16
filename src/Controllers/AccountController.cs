83d65b4ccfc14604dc9168cf86438bb3cd716b93
Drake Lambert <drake.askme2@gmail.com>
2018-04-16 14:23:20 -0500

    using return url

--- src/Controllers/AccountController.cs
+++ src/Controllers/AccountController.cs
@@ -27,14 +27,21 @@ namespace GoldenTicket.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        public async Task<IActionResult> Login([FromForm] LoginRequest loginRequest, [FromQuery] string returnUrl = null)
         {
             var result = await _signInManager.PasswordSignInAsync(loginRequest.Username, loginRequest.Password, loginRequest.RememberMe, false);
 
             if (result.Succeeded)
             {
                 _logger.LogInformation($"{User.Identity.Name} logged in.");
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction(nameof(TicketsController.All), "Tickets");
+                }
             }
 
             return View(loginRequest);
