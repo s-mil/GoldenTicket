2a6ad6fb527f876667231c9831cc8846a98b0629
Jeremy LeJeune <jleje33@lsu.edu>
2018-04-16 20:02:44 -0500

    added comments to token request

--- src/Models/TokenRequest.cs
+++ src/Models/TokenRequest.cs
@@ -1,9 +1,20 @@
 namespace GoldenTicket.Models
 {
+    /// <summary>
+    /// token request model
+    /// </summary>
     public class TokenRequest
     {
+        /// <summary>
+        /// Gets user's username
+        /// </summary>
+        /// <returns>username string</returns>
         public string Username { get; set; }
 
+        /// <summary>
+        /// Gets user's password
+        /// </summary>
+        /// <returns>password string</returns>
         public string Password { get; set; }
     }
 }
