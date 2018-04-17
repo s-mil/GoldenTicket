4de9326c5d56c41143799c7d5718a9fe1c8b586d
Drake Lambert <drake.askme2@gmail.com>
2018-04-17 00:01:52 -0500

    add ticket

--- src/Controllers/ClientsController.cs
+++ src/Controllers/ClientsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using GoldenTicket.Models.ClientsViewModels;
+using GoldenTicket.Models;
 
 namespace GoldenTicket.Controllers
 {
@@ -33,8 +34,32 @@ namespace GoldenTicket.Controllers
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var clients = await _context.Clients.GroupJoin(_context.Tickets.Where(ticket => ticket.Open), client => client.Id, ticket => ticket.ClientId, (client, tickets) => new ClientDetails { Client = client, Tickets = tickets.ToList() }).OrderByDescending(details => details.Tickets.Count).ToListAsync();
+            var clients = await _context.Clients.GroupJoin(_context.Tickets.Where(ticket => ticket.Open), client => client.Id, ticket => ticket.ClientId, (client, tickets) => new ClientDetails { Client = client, Tickets = tickets, OpenTicketCount = tickets.Count() }).OrderByDescending(details => details.Tickets.Count()).ToListAsync();
             return View(clients);
+        }
+
+        /// <summary>
+        /// Gets view for adding a ticket.
+        /// </summary>
+        /// <returns>The view.</returns>
+        [HttpGet]
+        public async Task<IActionResult> AddTicket([FromRoute] Guid id)
+        {
+            return View(new Ticket { ClientId = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTicket([FromForm] Ticket ticket)
+        {
+            ticket.DateAdded = DateTime.Now;
+            ticket.IsUrgent = false;
+            ticket.Open = true;
+
+            _context.Tickets.Add(ticket);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(TicketsController.Open), "Tickets", new { id = ticket.Id });
         }
     }
 }
