3eaa1850f40d5148a2c7ec9d2c832d0520640e8c
Drake Lambert <drake.askme2@gmail.com>
2018-04-17 17:59:10 -0500

    added constants

---
+++ src/Data/Constants.cs
@@ -0,0 +1,18 @@
+namespace GoldenTicket.Data
+{
+    /// <summary>
+    /// Constant variables for data use.
+    /// </summary>
+    public static class DataConstants
+    {
+        /// <summary>
+        /// The role of administrator.
+        /// </summary>
+        public const string AdministratorRole = "administrator";
+
+        /// <summary>
+        /// Administrator username
+        /// </summary>
+        public const string RootUsername = "admin";
+    }
+}
