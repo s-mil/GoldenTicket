192c3305850203807003d70319b86f1effcae95c
Jeremy LeJeune <jleje33@lsu.edu>
2018-04-16 19:15:42 -0500

    added comments to program

--- src/Program.cs
+++ src/Program.cs
@@ -10,13 +10,25 @@ using Microsoft.Extensions.Logging;
 
 namespace GoldenTicket
 {
+    /// <summary>
+    /// Our program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Entry point of program
+        /// </summary>
+        /// <param name="args">command line arguments</param>
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
+        /// <summary>
+        /// builds the web host
+        /// </summary>
+        /// <param name="args">command line arguments</param>
+        /// <returns>errors</returns>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
