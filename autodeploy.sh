2185898d9c4242dffe2bcb3d79dedae506a4d28e
Sam Miller <sithpow@gmail.com>
2018-04-18 15:57:44 -0500

    Update autodeploy.sh

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
+timestamp=$(date +%s)
+
+
 # Pull the repo and act on the boolean value created by grep
 git -C /home/gold/GoldenTicket fetch
 if git -C /home/gold/GoldenTicket pull | grep -Fxq 'Already up-to-date.'
   then
-      echo "Already up-to-date."
+      echo timestamp+"Already up-to-date."
   else
-    dotnet publish /home/gold/GoldenTicket/src
+    dotnet publish /home/gold/GoldenTicket/src &&
 
-    sudo cp -rf /home/gold/GoldenTicket/src/bin/Debug/netcoreapp2.0/publish/* /var/aspnetcore/GoldenTicket/
+    sudo cp -rf /home/gold/GoldenTicket/src/bin/Debug/netcoreapp2.0/publish/* /var/aspnetcore/GoldenTicket/ &&
 
-    sudo systemctl restart kestrel-golden-ticket
+    sudo systemctl restart kestrel-golden-ticket &&
+    
+    echo timestamp+"Redeploy Finished"
 fi
+
